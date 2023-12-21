b7bf7f734fd6e0b03daf14d0bbf2f1305c071035
Zachatoo <Zachatoo@users.noreply.github.com>
2023-12-21 09:06:35 -0700

    Add requestUrl example of uploading a file

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Plugin } from "obsidian";
+import { Plugin, TFile, requestUrl } from "obsidian";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface RequestExamplePluginSettings {}
@@ -10,6 +10,25 @@ export default class RequestExamplePlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
+
+		this.addCommand({
+			id: "request-url",
+			name: "Request URL",
+			callback: async () => {
+				const url = "https://google.com";
+				const path = "ogimage.png";
+				const file = this.app.vault.getAbstractFileByPath(path);
+				if (file instanceof TFile) {
+					const data = await this.app.vault.readBinary(file);
+					const res = await requestUrl({
+						url,
+						method: "POST",
+						body: data,
+					});
+					console.log("res", res);
+				}
+			},
+		});
 	}
 
 	onunload() {}
