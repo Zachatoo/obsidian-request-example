659fa9e3948b330e9431e4c9e54914c98e667de8
Zachatoo <Zachatoo@users.noreply.github.com>
2023-12-21 09:24:15 -0700

    FormData example (doesn't work)

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,28 @@ export default class RequestExamplePlugin extends Plugin {
 				}
 			},
 		});
+
+		this.addCommand({
+			id: "request-url-formdata",
+			name: "Request URL FormData (DOESN'T WORK)",
+			callback: async () => {
+				const url = "https://google.com";
+				const path = "ogimage.png";
+				const file = this.app.vault.getAbstractFileByPath(path);
+				if (file instanceof TFile) {
+					const data = await this.app.vault.readBinary(file);
+					const blob = new Blob([data], { type: "image/png" });
+					const formData = new FormData();
+					formData.set("file", blob, path);
+					const res = await requestUrl({
+						url,
+						method: "POST",
+						body: formData,
+					});
+					console.log("res", res);
+				}
+			},
+		});
 	}
 
 	onunload() {}
